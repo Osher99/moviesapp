0c01592bb376bd69af1eddee88007aac639394ec
Osher Dror <osherd25@gmail.com>
2023-05-01 00:29:21 +0300

    Mobile fixes

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -5,12 +5,14 @@ import { setVoteAverageClass } from './utils';
 import IMDB from '../../assets/imdb.svg';
 import Y2S from '../../assets/yts-mx.svg';
 
-const Movie = ({ title, poster_path, overview, vote_average }) =>(
+const Movie = ({ title, poster_path, overview, vote_average }) => {
+    const voteRounded = Math.round(vote_average);
+    return (
     <div className="movie">
         <img className="poster" src={poster_path ? IMGPATH + poster_path : BLANKIMGPATH} alt={title} />
         <div className="movie-info">
             <h2>{title}</h2>
-            <span className={`tag ${setVoteAverageClass(vote_average)}`}>{vote_average === 0 ? 'N/A' : vote_average}</span>
+            <span className={`tag ${setVoteAverageClass(voteRounded)}`}>{voteRounded === 0 ? 'N/A' : voteRounded}</span>
         </div>
         <div className="movie-over">
             <h2>Overview:</h2>
@@ -21,6 +23,7 @@ const Movie = ({ title, poster_path, overview, vote_average }) =>(
             </div>
         </div>
     </div>
-);
+    )
+};
 
 export default Movie;
